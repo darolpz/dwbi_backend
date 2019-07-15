a989b7efdefba025014f5d5de15af20686bff968
Dario Lopez <daropl12@gmail.com>
2019-07-15 00:29:41 -0300

    actualizando branc

--- dist/controllers/api.js
+++ dist/controllers/api.js
@@ -11,6 +11,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 }
 Object.defineProperty(exports, "__esModule", { value: true });
+const exceljs_1 = require("exceljs");
 const __1 = __importDefault(require(".."));
 const DBC = require('../dbconfig');
 class ApiController {
@@ -39,6 +40,62 @@ class ApiController {
             INNER JOIN ${DBC.dbconfig.database}.LK_Vehiculo v ON v.IdVehiculo = cv.IdVehiculo 
             GROUP BY v.Descipcion 
             HAVING AVG(MinutosViaje) > 0;`;
+            const result = yield this.select(query);
+            res.json({
+                status: 200,
+                result: result,
+            });
+        });
+    }
+    generateExceltimeForvehicle(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            var workbook = new exceljs_1.Workbook();
+            console.log(workbook);
+            var sheet = workbook.addWorksheet('Tiempo de viaje'); //creating worksheet
+            var data = yield this.select(`SELECT v.Descipcion AS Vehiculo, AVG(MinutosViaje) AS Tiempo 
+            FROM ${DBC.dbconfig.database}.CuboViajes cv 
+            INNER JOIN Datawarehouse.LK_Vehiculo v ON v.IdVehiculo = cv.IdVehiculo 
+            GROUP BY v.Descipcion 
+            HAVING AVG(MinutosViaje) > 0;`);
+            var encabezados = ['Vehiculo', 'Minutos promedio'];
+            sheet.addRow(encabezados);
+            data.forEach(x => {
+                sheet.addRow([x.Vehiculo, x.Tiempo]);
+            });
+            var tempfile = require('tempfile');
+            var tempFilePath = tempfile('.xlsx');
+            console.log("tempFilePath : ", { root: '/tmp/' }, tempFilePath);
+            workbook.xlsx.writeFile(tempFilePath).then(function () {
+                res.sendFile(tempFilePath, function (err) {
+                    if (!!err) {
+                        console.log('---------- error downloading file: ', err);
+                        res.status(500).end();
+                    }
+                    else {
+                        console.log("Todo OK");
+                    }
+                });
+            });
+        });
+    }
+    cantidadPorTurno(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const query = `SELECT e.Horario, COUNT(*) AS Cantidad FROM ${DBC.dbconfig.database}.CuboViajes cv 
+            INNER JOIN ${DBC.dbconfig.database}.LK_Empleado e ON e.DNI = cv.DNIEmpleado
+            GROUP BY e.Horario;`;
+            const result = yield this.select(query);
+            res.json({
+                status: 200,
+                result: result,
+            });
+        });
+    }
+    cantidadPorEmpleado(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const query = `SELECT e.Nombre, e.DNI, COUNT(*) AS Cantidad 
+            FROM ${DBC.dbconfig.database}.CuboViajes cv
+            INNER JOIN ${DBC.dbconfig.database}.LK_Empleado e ON e.DNI = cv.DNIEmpleado
+            GROUP BY e.DNI, e.Nombre;`;
             const result = yield this.select(query);
             res.json({
                 status: 200,
--- dist/routes/routes.js
+++ dist/routes/routes.js
@@ -10,4 +10,7 @@ const apiCtrl = new api_1.default();
 routes.route('/').get(apiCtrl.index);
 routes.route('/home').get(apiCtrl.home);
 routes.route('/timeForvehicle').get(apiCtrl.timeForvehicle.bind(apiCtrl));
+routes.route('/timeForvehicle-download').get(apiCtrl.generateExceltimeForvehicle.bind(apiCtrl));
+routes.route('/cantidadPorEmpleado').get(apiCtrl.cantidadPorEmpleado.bind(apiCtrl));
+routes.route('/cantidadPorTurno').get(apiCtrl.cantidadPorTurno.bind(apiCtrl));
 exports.default = routes;
